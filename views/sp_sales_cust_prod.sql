21d256ea9a8aa029b7b798758f415ea713e18a61
Amit Kumar singh <amitcs010@gmail.com>
2025-12-09 15:44:17 +0530

    update

--- views/sp_sales_cust_prod.sql
+++ views/sp_sales_cust_prod.sql
@@ -1,20 +1,20 @@
 CREATE OR REPLACE VIEW vw_sales_customer_product AS
 SELECT 
-    s.sale_id,
-    s.sale_date,
-    s.quantity,
-    s.total_value,
+    sl.sale_id,
+    sl.sale_date,
+    sl.quantity,
+    sl.total_value,
 
-    c.customer_id,
-    c.first_name,
-    c.last_name,
-    c.region,
+    cl.customer_id,
+    cl.first_name,
+    cl.last_name,
+    cl.region,
 
     p.product_id,
     p.product_name,
     p.category
-FROM sales_strat_plan.sales s
-LEFT JOIN cust_mstr.customers c 
-       ON s.customer_id = c.customer_id
+FROM sales_strat_plan.sales sl
+LEFT JOIN cust_mstr.customers cl 
+       ON sl.customer_id = cl.customer_id
 LEFT JOIN matrl_mstr.products p
-       ON s.product_id = p.product_id;
+       ON sl.product_id = p.product_id;
