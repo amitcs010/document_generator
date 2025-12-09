ecb69cb58315b0d86ad8105475ebfca330357ded
Amit Kumar singh <amitcs010@gmail.com>
2025-12-09 15:47:48 +0530

    updated the analytics view

--- views/sp_sales_cust_prod.sql
+++ views/sp_sales_cust_prod.sql
@@ -10,11 +10,11 @@ SELECT
     cl.last_name,
     cl.region,
 
-    p.product_id,
-    p.product_name,
-    p.category
+    pd.product_id,
+    pd.product_name,
+    pd.category
 FROM sales_strat_plan.sales sl
 LEFT JOIN cust_mstr.customers cl 
        ON sl.customer_id = cl.customer_id
-LEFT JOIN matrl_mstr.products p
-       ON sl.product_id = p.product_id;
+LEFT JOIN matrl_mstr.products pd
+       ON sl.product_id = pd.product_id;
